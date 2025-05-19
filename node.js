5fe290433cc5c0731945c73abcee8397a794e0a4
SourCandyLime <taggertallred@gmail.com>
2025-05-19 16:15:49 +0000

    updated main site docs links

--- node.js
+++ node.js
@@ -20,15 +20,6 @@ app.get('/', (req, res) => res.render('index', { title: 'Home' }));
 app.get('/about', (req, res) => res.render('about', { title: 'About' }));
 app.get('/docs', (req, res) => res.render('docs', { title: 'Documentation'}));
 
-app.get('/docs/forum', (req, res) => res.render('subdocs/forumfiller', { title: 'Forum (Placeholder)'}));
-app.get('/docs/notes', (req, res) => res.render('subdocs/notes', { title: 'Notes (Placeholder)'}));
-
-app.get('/docs/official', (req, res) => {
-  const docsDir = path.join(__dirname, 'public', 'docs', 'official');
-  const files = fs.readdirSync(docsDir).filter(file => file.endsWith('.pdf'));
-  res.render('subdocs/official', { title: 'Official Docs', files}); // ✅ Add 'files' here
-});
-
 // dev route - dynamically loads dev posts from /devlogs folder
 app.get('/dev', (req, res) => {
   const logs = fs.readdirSync('./devlogs').filter((f) => f.endsWith('.md'));
